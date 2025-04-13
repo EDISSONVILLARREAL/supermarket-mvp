afe0b6f5e3af676ecfd405fa7060cecd5c59685e
Edisson Ferney Villarreal <cuentacecep@gmail.com>
2025-04-12 23:10:07 -0500

    Main View

---
+++ Presenters/Common/ModelDataValidation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_mvp.Presenters.Common
+{
+    internal class ModelDataValidation
+    {
+        public void Validate(object model)
+        {
+            string errorMessage = "";
+            var validationResults = new List<ValidationResult>();
+            var validationContext = new ValidationContext(model);
+            bool isValid = Validator.TryValidateObject(
+                model, validationContext, validationResults, true);
+
+            if (!isValid)
+            {
+                foreach (var item in validationResults)
+                {
+                    errorMessage += item.ErrorMessage + "\n";
+                }
+
+                throw new Exception(errorMessage);
+            }
+        }
+    }
+}
--- Presenters/PayModePresenter.cs
+++ Presenters/PayModePresenter.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Supermarket_mvp.Views;
 using Supermarket_mvp.Models;
 using static Supermarket_mvp.Views.PayModeView;
+using Microsoft.VisualBasic.Logging;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 
 
@@ -71,18 +73,50 @@ namespace Supermarket_mvp.Presenters
 
         private void SavePayMode(object? sender, EventArgs e)
         {
-            throw new NotImplementedException();
-        }
+// Se crea un objeto de la clase PayModeModel y se asignan los datos
+    // De las cajas de texto de la vista
+    var payMode = new PayModeModel();
+            payMode.Id = Convert.ToInt32(view.PayModeId);
+            payMode.Name = view.PayModeName;
+            payMode.Observation = view.PayModeObservation;
 
-        private void DeleteSelectedPayMode(object? sender, EventArgs e)
-        {
-            throw new NotImplementedException();
-        }
+            try
+            {
+                if (view.IsEdit)
+                {
+                    repository.Edit(payMode);
+                    view.Message = "PayMode edited successfully";
+                }
+                else
+                {
+                    repository.Add(payMode);
+                    view.Message = "PayMode added successfully";
+                }
+            }
+            catch (Exception ex)
+            {
+                // Si ocurre una excepción se configura IsSuccesfull en false
+                // y a la propiedad Message de la vista se asigna el mensaje de la excepción
+                view.IsSuccessful = false;
+                view.Message = ex.Message;
+            }
+        }        
 
         private void LoadSelectPayModeToEdit(object? sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            // Se obtiene el objeto del datagridview que se encuentra seleccionado
+            var payMode = (PayModeModel)payModeBindingSource.Current;
+
+            // Se cambia el contenido de las cajas de texto por el objeto recuperado del datagridview
+            view.PayModeId = payMode.Id.ToString();
+            view.PayModeName = payMode.Name;
+            view.PayModeObservation = payMode.Observation;
+
+            // Se establece el modo como edición
+            view.IsEdit = true;
         }
+
+        
     }
 }
 
